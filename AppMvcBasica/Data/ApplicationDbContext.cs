38c8ea7d815330429550efc5b4e5559e0a000204
David Oliveira <luiz.david@cencosud.com.br>
2020-08-23 21:46:10 -0300

    Versão 1.01

---
+++ AppMvcBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppMvcBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+//Contexto é do identity mas pode se utilizado também para o nossa aplicação 
+//Não é recomendado utilizar em aplicações divididas em camadas
+namespace AppMvcBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
